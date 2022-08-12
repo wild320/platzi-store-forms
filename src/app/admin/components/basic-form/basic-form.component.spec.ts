261b81801b1a1842bfc47b4cf60f7a0cc581e933
wild320 <wildesjaramillo@gmail.com>
2022-08-12 12:00:52 -0500

    ang11

--- src/app/admin/components/basic-form/basic-form.component.spec.ts
+++ src/app/admin/components/basic-form/basic-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BasicFormComponent } from './basic-form.component';
 
@@ -6,7 +6,7 @@ describe('BasicFormComponent', () => {
   let component: BasicFormComponent;
   let fixture: ComponentFixture<BasicFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BasicFormComponent ]
     })
